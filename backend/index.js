2ab40e823b512e0c4010bf09e01697ef8ee5c94a
Nehareddy1711 <nehaatham@lyrostech.com>
2024-07-02 14:39:09 +0530

    upload

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // Database Connection with MongoDB
-const dbUri = "mongodb+srv://nehareddy1711:Ammu%40897@cluster0.akczrww.mongodb.net/mydatabase?retryWrites=true&w=majority";
+const dbUri = "mongodb+srv://nehareddy1711:Ammu%40897@cluster0.akczrww.mongodb.net/e-commerce?retryWrites=true&w=majority";
 
 mongoose.connect(dbUri, {
     useNewUrlParser: true,
@@ -45,6 +45,89 @@ app.post("/upload",upload.single('product'),(req,res)=>{
     })
 })
 
+// Schema for Creating Products
+
+const Product = mongoose.model("Product",{
+    id:{
+        type: Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avilable:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async (req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+        {
+            let last_product_array = products.slice(-1);
+            let last_product = last_product_array[0];
+            id = last_product.id+1;
+        }
+        else{
+            id=1;
+        }
+    const product = new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price, 
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+//Creating API for deleting Products
+
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+// Creating API for getting all products
+app.get('/allproducts',async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server Running on Port " + port);
